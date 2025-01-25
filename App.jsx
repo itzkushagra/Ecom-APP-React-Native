d4084eb098b1533b647e3b2c28a0648ba0cf2a79
itzkushagra <info.kushagrapandey@gmail.com>
2025-01-25 23:10:06 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import HomePagr from './pages/HomePagr';
+
+const App = () => {
+  return (
+    <HomePagr/>
+  )
+}
+
+export default App;
---
+++ pages/HomePagr.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+const HomePagr = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hi there whats up?????</Text>
+      </View>
+    </NavigationContainer>
+  )
+}
+
+export default HomePagr
+
+const styles = StyleSheet.create({})
